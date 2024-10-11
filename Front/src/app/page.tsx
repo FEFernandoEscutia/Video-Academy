67758252d09974cd8b8da8f6faf808f088b18b70
Monacada16 <juanxomoncada@gmail.com>
2024-10-11 18:59:48 -0500

    cambios review

--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -1,14 +1,23 @@
+import UserReview from "@/components/UserReview/UserReviews";
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
+
   return (
     <>
+    
     <button className="primary-btn">
       Explorar cursos
     </button>
     <button className="secondary-btn">
       Explorar cursos
     </button>
+    <section className="mt-8">
+      <UserReview/>
+    </section>
+    
     </>
+    
   );
 }

0b878a3e4bd2c3a460fca1aa0a025bdba3027417
Araneus <kbasara69@gmail.com>
2024-10-20 12:04:24 -0500

    Agregada documentacion Swagger de dtos de update

--- Back/src/modules/video/dto/update-video.dto.ts
+++ Back/src/modules/video/dto/update-video.dto.ts
@@ -1,4 +1,34 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateVideoDto } from './create-video.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateVideoDto extends PartialType(CreateVideoDto) {}
+export class UpdateVideoDto extends PartialType(CreateVideoDto) {
+  @ApiProperty({
+    description: 'Title of the video',
+    example: 'Understanding NestJS Basics',
+    required: false,
+  })
+  title?: string;
+
+  @ApiProperty({
+    description: 'Description of the video content',
+    example:
+      'This video explains the core concepts of NestJS, a progressive Node.js framework.',
+    required: false,
+  })
+  description?: string;
+
+  @ApiProperty({
+    description: 'URL of the video',
+    example: 'https://www.example.com/video/understanding-nestjs-basics',
+    required: false,
+  })
+  url?: string;
+
+  @ApiProperty({
+    description: 'ID of the course associated with the video',
+    example: 'course123',
+    required: false,
+  })
+  courseId?: string;
+}

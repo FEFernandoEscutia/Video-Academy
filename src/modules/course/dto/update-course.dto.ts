299dff6b9e872f4b762902bcbbd1e7b16c1c54e1
FEFernandoEscutia <134465084+FEFernandoEscutia@users.noreply.github.com>
2024-11-04 12:44:31 -0600

    fixing the reviews all endpoin

--- src/modules/course/dto/update-course.dto.ts
+++ src/modules/course/dto/update-course.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCourseDto } from './create-course.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateCourseDto extends PartialType(CreateCourseDto) {
-    
+  @IsBoolean()
+  @IsOptional()
+  isAvailable?: boolean;
 }
